9e9b0f8ad38053f6a3d56ff0debfeb8d94af7c79
yulei <yulei5024@gmail.com>
2024-11-30 14:44:56 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch dev # Your branch is up to date with 'origin/dev'. # # Changes to be committed: #	new file:   auto-special-rule/openai.yaml #	modified:   openclash-special-rules.sh #

--- openclash-special-rules.sh
+++ openclash-special-rules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-OUTPUT_DIR="/root/Personal-project/openclash-rule/auto-special-rule/"
-GIT_REPO_DIR="/root/Personal-project/openclash-rule/"
+OUTPUT_DIR="/root/Personal-project/openclash-special-rule/openclash-rule/auto-special-rule/"
+GIT_REPO_DIR="/root/Personal-project/openclash-special-rule/openclash-rule"
 
 cd "$GIT_REPO_DIR" || { log "Failed to change directory to: $GIT_REPO_DIR"; exit 1; }
 if git pull; then
