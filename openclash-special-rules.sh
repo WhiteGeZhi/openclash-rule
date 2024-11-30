d3b6dfeae9672c2e3ef1b58f80c62b1763f01ceb
yulei <yulei5024@gmail.com>
2024-11-30 14:41:00 +0800

    增加特殊规则下载覆盖

---
+++ openclash-special-rules.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OUTPUT_DIR="/root/Personal-project/openclash-rule/auto-special-rule/"
+GIT_REPO_DIR="/root/Personal-project/openclash-rule/"
+
+cd "$GIT_REPO_DIR" || { log "Failed to change directory to: $GIT_REPO_DIR"; exit 1; }
+if git pull; then
+  log "Git pull successful"
+else
+  log "Git pull failed"
+  exit 1
+fi
